ad11913c18c37ad6e30031eab0825eb829954edb
Sorniak <Sorniak@net.tut>
2013-07-10 21:51:32 +0000

    Altered web.js

--- web.js
+++ web.js
@@ -2,12 +2,14 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var buf = fs.readFileSync("/index.html");
-var index = buf.toString();
+
 
 app.get('/', function(request, response) {
-  
-  response.send(index);
+
+var fs = require('fs');  
+var buf = new Buffer(fs.readFileSync('index.html'), 'utf-8');
+
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
