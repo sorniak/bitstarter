13fce58f949480c9645aff02a56fb13b043fe0d2
Sorniak <Sorniak@net.tut>
2013-07-10 19:59:06 +0000

    index to hello world

--- web.js
+++ web.js
@@ -2,8 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var buf = fs.readFileSync("/index.html");
+var index = buf.toString();
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  
+  response.send(index);
 });
 
 var port = process.env.PORT || 5000;
